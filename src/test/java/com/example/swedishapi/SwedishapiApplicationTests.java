82d2a3a53fd5dcaa93dc0c760ef623f485fb7aa9
Dimitrios Tikas <dimitrios.tikas@cpacsystems.se>
2020-02-18 21:16:38 +0100

    first commit

---
+++ src/test/java/com/example/swedishapi/SwedishapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.swedishapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SwedishapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
